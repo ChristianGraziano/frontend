d2c0de1499232eab7fb24711cc98e39f7c58db42
ChristianGraziano <123503135+ChristianGraziano@users.noreply.github.com>
2023-08-30 19:12:55 +0200

    primo commit

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>oooops thi page not exist!!!</div>;
+};
+
+export default ErrorPage;
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
+
+const Homepage = () => {
+  return (
+    <>
+      <NavigationBar />
+    </>
+  );
+};
+
+export default Homepage;
